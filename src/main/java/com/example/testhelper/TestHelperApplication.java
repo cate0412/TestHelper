bb46d015cb97d26b9699ad0f9287eeced0da009b
jinyeonseo <eogml412@gmail.com>
2024-04-24 11:14:35 +0900

    git init

---
+++ src/main/java/com/example/testhelper/TestHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.testhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testhelper/TestHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
