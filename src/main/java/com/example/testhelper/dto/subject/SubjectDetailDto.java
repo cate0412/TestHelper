c31146b1dfd849108540cb6dc826461cec1282b8
jinyeonseo <eogml412@gmail.com>
2024-04-28 15:33:54 +0900

    fix: SubjectDetailDto 수정

--- src/main/java/com/example/testhelper/dto/subject/SubjectDetailDto.java
+++ src/main/java/com/example/testhelper/dto/subject/SubjectDetailDto.java
@@ -1,23 +1,24 @@
 package com.example.testhelper.dto.subject;
 
 import com.example.testhelper.domain.subject.Subject;
+import com.example.testhelper.dto.test.TestResultDto;
+import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
-public class SubjectInfoDto {
-
+public class SubjectDetailDto {
     private Integer id;
-    private String code;
     private String name;
-    private Integer price;
     private Integer validDay;
+    private List<TestResultDto> testResultList;
 
-    public SubjectInfoDto(Subject subject) {
+    @Builder
+    public SubjectDetailDto(Subject subject, List<TestResultDto> testResultList) {
         this.id = subject.getId();
-        this.code =  subject.getCode();
         this.name = subject.getName();
-        this.price = subject.getPrice();
         this.validDay = subject.getValidDay();
+        this.testResultList = testResultList;
     }
-
 }
